dd19d2aaaf4a1761840a3ea5c896fa69ac817369
Jaime <jaalorsa517@hotmail.com>
2021-04-21 22:41:43 -0500

    todo listo para produccion

---
+++ dist/precache-manifest.e4126b507eea0e7e99097fd9b0430907.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a99b31d901673e5970d",
+    "url": "/css/app.088fd952.css"
+  },
+  {
+    "revision": "638ce1bc4ad342098288",
+    "url": "/css/chunk-7e333ad1.a70f3ebe.css"
+  },
+  {
+    "revision": "c792243a8eea801940f1",
+    "url": "/css/chunk-a1d715ac.4a13629e.css"
+  },
+  {
+    "revision": "e47bcc037d9a9ef03609",
+    "url": "/css/chunk-vendors.8b220ae0.css"
+  },
+  {
+    "revision": "50dfda710abf6f0197eedefdfe152c2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a99b31d901673e5970d",
+    "url": "/js/app.133fd978.js"
+  },
+  {
+    "revision": "638ce1bc4ad342098288",
+    "url": "/js/chunk-7e333ad1.5eed14d4.js"
+  },
+  {
+    "revision": "c792243a8eea801940f1",
+    "url": "/js/chunk-a1d715ac.e00adb6e.js"
+  },
+  {
+    "revision": "e47bcc037d9a9ef03609",
+    "url": "/js/chunk-vendors.e8350389.js"
+  },
+  {
+    "revision": "8103cfa140122fe8cec94a559a34c677",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
